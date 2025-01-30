422276ce19e94f070a8d5563239dfa3e31e4e310
Lucas Santos <santos.lucas1029@gmail.com>
2025-01-30 10:30:36 -0300

    feat: criação de função de adicionar amigos

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeAmigos = [];
+
+function adicionarAmigo() {
+    let campoAmigo = document.getElementById('amigo');
+    let nome = campoAmigo.value;
+    // console.log(nome);
+    nome == '' ? alert('Por favor, insira um nome') : listaDeAmigos.push(nome);
+    console.log(listaDeAmigos);
+    campoAmigo.value = '';
+}
