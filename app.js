f040efe91a356dfab89924ee5123bfc2b0b5f1f1
Lucas Santos <santos.lucas1029@gmail.com>
2025-01-30 10:32:55 -0300

    feat: criação de função para exibir a lista de amigos na tela

--- app.js
+++ app.js
@@ -7,4 +7,18 @@ function adicionarAmigo() {
     nome == '' ? alert('Por favor, insira um nome') : listaDeAmigos.push(nome);
     console.log(listaDeAmigos);
     campoAmigo.value = '';
+    listarAmigos();
+}
+
+function listarAmigos() {
+    let campoLista = document.getElementById('listaAmigos');
+    // campoLista.innerHTML = '';
+    console.log('lista vazia');
+
+    for (i = 0; i <= listaDeAmigos.length; i++) {
+        let itemLista = document.createElement('li');
+        itemLista.value = listaDeAmigos[i];
+        campoLista.appendChild(itemLista);
+        campoLista.innerHTML = itemLista.value;
+    }
 }
